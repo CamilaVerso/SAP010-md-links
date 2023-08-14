18c10ac03932e8db34b74f5efe62d24beb58b0bb
CamilaVerso <camilaverso2@gmail.com>
2023-08-14 16:34:55 -0300

    Configurando o markdown-link-extractor e iniciando a função

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs').promises;
+const path = require('path');
+const extract = require('markdown-link-extractor');
+
+function mdLinks(rotaRelativa) {
+  const rotaAbsoluta = path.resolve(rotaRelativa);
+
+  return new Promise((resolve, reject) => {
+    fs.access(rotaAbsoluta, fs.constants.F_OK, (err) => {
+      if (err) {
+        reject('A rota não existe!');
+        return;
+      }
+
+      const extensoesMarkdown = ['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text'];
+      const extensaoArquivo = path.extname(rotaAbsoluta);
+
+      if (!extensoesMarkdown.includes(extensaoArquivo)) {
+        reject('O arquivo não é Markdown!');
+        return;
+      }
+
+      fs.readFile(rotaAbsoluta, 'utf-8')
+        .then(conteudo => {
+          const linksEncontrados = extract(conteudo);
+          resolve(linksEncontrados);
+        })
+        .catch(err => {
+          reject(`Erro ao ler o arquivo: ${err}`);
+        });
+    });
+  });
+}
+// Exemplo de uso
+mdLinks('./README.md')
+  .then(links => {
+    console.log('Links encontrados:', links);
+  })
+  .catch(err => {
+    console.error('Erro:', err);
+  })
